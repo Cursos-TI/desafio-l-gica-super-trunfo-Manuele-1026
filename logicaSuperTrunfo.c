cd341ba2e47b1a5886edd78962308cdf8e6fdee9
Manuele-1026 <manuelesouza1026@gmail.com>
2025-06-07 22:21:52 +0000

    Início

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int main ()
+
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
